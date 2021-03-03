b5ba8cf25a009da1a70f96023e80f8bffed89145
SHIMA Tatsurou <shima.tatsurou@gmail.com>
2021-03-03 14:41:13 +0900

    modify describe 一覧表示機能 with change order of task list

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
+require 'helpers/basic_auth_helper.rb'
+
 RSpec.describe 'タスク管理機能', type: :system do
 
   describe '新規作成機能' do
+    before do
+      @label_index=I18n.t('tasks.label_index')
+      @label_show=I18n.t('tasks.label_show')
+      @label_name=I18n.t('tasks.label_name')
+      @label_description=I18n.t('tasks.label_description')      
+      @label_submit_new=I18n.t('tasks.label_submit_new')
+
+      #BASIC認証を通過
+      visit_with_http_auth tasks_path
+    end
+    
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
 
@@ -9,18 +22,18 @@ RSpec.describe 'タスク管理機能', type: :system do
         visit new_task_path
 
         # 2. 新規登録内容を入力する
-        fill_in 'Name', with: "task-1"
-        fill_in 'Description', with: "コンテント１"
+        fill_in "new_task_name", with: "task-1"
+        fill_in "new_task_description", with: "コンテント１"
 
         # 3. 「Create Task」というvalue（表記文字）のあるボタンをクリックする
-        click_on 'Create Task'
+        click_on "new_task_submit"
 
         # 4. clickで登録された情報が、タスク確認ページに表示されているかを確認する
         expect(page).to have_content "task-1"
         expect(page).to have_content "コンテント１"
 
         # 5. 「登録する」というvalue（表記文字）のあるボタンをクリックする
-        click_on '登録する'
+        click_on "confirm_task_submit"
 
         # 6. clickで登録された情報が、タスク詳細ページに表示されているかを確認する
         expect(page).to have_content "task-1"
@@ -31,10 +44,46 @@ RSpec.describe 'タスク管理機能', type: :system do
   end
 
   describe '一覧表示機能' do
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        FactoryBot.create(:task, name: 'task-1', description: 'description-1')
+        FactoryBot.create(:task, name: 'task-2', description: 'description-2')
+
+        #新規画面で項目入力
+        visit new_task_path
+        expect(page).to have_content @label_new
+        fill_in "new_task_name", with: 'task-3'
+        fill_in "new_task_description", with: 'description-3'
+
+        #登録ボタン押下
+        click_on "new_task_submit"
+
+        #遷移した確認画面で登録ボタン押下
+        expect(page).to have_content @label_confirm
+        click_on "confirm_task_submit"
+
+        #遷移した一覧画面の表示項目を取得
+        expect(page).to have_content @label_index
+        task_list_name = all('.task_row_name')
+        task_list_description = all('.task_row_description')
+
+        #一番上の表示項目が作成日時のあたらしい'task-1'/'description-1'であるかをチェック
+        #expect(task_list_name.count).to eq 3
+        #expect(task_list_description.count).to eq 3
+        expect(task_list_name[0].text).to eq 'task-3'
+        expect(task_list_name[1].text).to eq 'task-2'
+        expect(task_list_name[2].text).to eq 'task-1'
+        expect(task_list_description[0].text).to eq 'description-3'
+        expect(task_list_description[1].text).to eq 'description-2'
+        expect(task_list_description[2].text).to eq 'description-1'                
+
+      end
+    end
+    
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
         #テストで使用するためのタスクを作成
-        task = FactoryBot.create(:task, name: 'task-2')
+        task = FactoryBot.create(:task)
 
         #タスク一覧ページに遷移
         visit tasks_path
@@ -52,15 +101,14 @@ RSpec.describe 'タスク管理機能', type: :system do
      context '任意のタスク詳細画面に遷移した場合' do
       it '該当タスクの内容が表示される' do
         #テストで使用するためのタスクを作成
-        task = FactoryBot.create(:task, name: 'task-3')
+        task = FactoryBot.create(:task)
 
         #タスク詳細ページに遷移
         visit task_path(task.id)
 
-        expect(page).to have_content '詳細画面'
+        expect(page).to have_content @label_show
         expect(page).to have_content task.name
         expect(page).to have_content task.description
-        # expectの結果が true ならテスト成功、false なら失敗として結果が出力される
 
       end
      end
