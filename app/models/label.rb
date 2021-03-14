c5e1688e1b794945d650eb2354cb1fc0e74aab9a
SHIMA Tatsurou <shima.tatsurou@gmail.com>
2021-03-14 15:25:51 +0900

    added multi-primary key setting to labelling model

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,5 +1,5 @@
 class Label < ApplicationRecord
   has_many :labellings, dependent: :destroy
   has_many :tasks, through: :labellings
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
 end
