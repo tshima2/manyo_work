8a0a1b099a30f39e395bd2bd4d5e7e89966e0082
SHIMA Tatsurou <shima.tatsurou@gmail.com>
2021-03-14 15:30:18 +0900

    add filter task by assoriated label function

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,15 +1,17 @@
 class Task < ApplicationRecord
   has_many :labellings, dependent: :destroy
   has_many :labels, through: :labellings
+
+
   enum priority: { 低: 0, 中: 1, 高: 2 }
-#  enum priority: { t('tasks.enum_priority_low'): 0, t('tasks.enum_priority_normal'): 1, t('tasks.enum_priority_high'): 2 }  
   validates :name, presence: true, uniqueness: true
   belongs_to :user
 
+  scope :label_search, -> (_label_id) { joins(:labellings).merge(Labelling.label_is _label_id) if _label_id.present? }
   scope :status_search, ->(_status) { where(status: _status) if _status.present? }
   scope :name_like, ->(_name){ where('name like ?', "%#{_name}%") if _name.present? }
 
-=begin  
+=begin
   scope :created_sort, ->(_flg){ order(created_at: :desc) if _flg }
   scope :expired_sort, ->(_flg){ order(deadline: :asc) } if _flg }
   scope :priority_sort, ->(_flg){ order(priority: :desc) } if _flg }
