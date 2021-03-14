bb6381398bf2d3d72cb1b683375c2268d51f7373
SHIMA Tatsurou <shima.tatsurou@gmail.com>
2021-03-14 15:38:07 +0900

    added Label/Labelling model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_13_020252) do
+ActiveRecord::Schema.define(version: 2021_03_14_033637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_03_13_020252) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["label_id"], name: "index_labellings_on_label_id"
+    t.index ["task_id", "label_id"], name: "index_labellings_on_task_id_and_label_id", unique: true
     t.index ["task_id"], name: "index_labellings_on_task_id"
   end
 
