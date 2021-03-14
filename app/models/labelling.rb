03b277e7177135fd2d518bf8f46e6372a8abd092
SHIMA Tatsurou <shima.tatsurou@gmail.com>
2021-03-14 15:27:23 +0900

    added multi-primary key setting, and filter task by label function

--- app/models/labelling.rb
+++ app/models/labelling.rb
@@ -1,4 +1,7 @@
 class Labelling < ApplicationRecord
   belongs_to :label
   belongs_to :task
+  validates :task_id, uniqueness: { scope: [:label_id] }
+  
+  scope :label_is, ->(_id) { where(label_id: _id) }
 end
